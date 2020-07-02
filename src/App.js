f725d4b6b0aa9459bb9f3a7dc61dfaeea1372304
ryand67 <rwdevenney@gmail.com>
2020-07-01 21:54:40 -0400

    Search bugs

--- src/App.js
+++ src/App.js
@@ -8,12 +8,14 @@ class App extends Component {
 
   state = { 
     employees: [],
-    search: ""
+    search: "",
+    renderList: []
   }
 
   componentDidMount() {
     API.search().then(res => {
-      this.setState({ employees: res.data })
+      this.setState({ employees: res.data });
+      this.setState({ renderList: res.data });
     });
   }
   callASDF = () => {
@@ -22,16 +24,25 @@ class App extends Component {
 
   handleChange = (event) => {
     this.setState({ search: event.target.value });
+    this.employeeSearch();
+  }
+
+  employeeSearch = () => {
     let newList = this.state.employees.filter(employee => {
       let fullName = `${employee.first} ${employee.last}`;
-      if(fullName.substr(0, this.state.search.length) === this.state.search.substr(0, this.state.search.length + 1)) {
+      if(fullName.toLowerCase().substr(0, this.state.search.length) === this.state.search.toLowerCase().substr(0, this.state.search.length)) {
         return true;
       } else {
         return false;
       }
     })
-    this.setState({ employees: newList });
-    console.log(newList);
+    this.setState({ renderList: newList });
+  }
+
+  sortEmployees = () => {
+    console.log('asdf');
+    let newList = this.state.renderList.sort();
+    this.setState({ renderList: newList })
   }
 
 
@@ -42,11 +53,11 @@ class App extends Component {
         <h1 className="text-center mt-5">EMPLOYEE DIRECTORY</h1>
         <SearchBar handleChange={this.handleChange}/>
         <div className="listHeader d-flex justify-content-around text-center">
-          <h2 className="listHeading">Name</h2>
+          <h2 className="listHeading nameHeading" onClick={this.sortEmployees}>Name</h2>
           <h2 className="listHeading">Email</h2>
           <h2 className="listHeading">Address</h2>
         </div>
-        {this.state.employees.map(item => {
+        {this.state.renderList.map(item => {
           let fullName = `${item.first} ${item.last}`
           return <ListItem name={fullName} key={fullName} email={item.email} address={item.address} />
         })}
